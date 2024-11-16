77a58810e626be27e020a658dbc1fea6294e9928
Kevin Collins <kevinmscollins@gmail.com>
2024-11-16 12:16:05 +0000

    feat: added React Frontend template

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Map from './components/Map'; // Import the Map component
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mapbox Integration</h1>
+      <Map /> {/* Render the Map component */}
+    </div>
+  );
+}
+
+export default App;
