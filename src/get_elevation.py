8cbb448c5c6ad6d43d874b0d268228f8ebf84bc7
Kevin Collins <kevinmscollins@gmail.com>
2025-01-06 16:17:49 +0000

    lint

--- src/get_elevation.py
+++ src/get_elevation.py
@@ -5,6 +5,7 @@ import numpy as np
 elevation_data_path = 'data/elevation_data.csv'  # Update the path to your file
 elevation_df = pd.read_csv(elevation_data_path)
 
+
 def find_closest_elevation(easting, northing):
     """
     Find the closest elevation data for a specific Easting and Northing.
@@ -19,8 +20,7 @@ def find_closest_elevation(easting, northing):
 
     # Calculate the Euclidean distance to all points in the DataFrame
     elevation_df['Distance'] = np.sqrt(
-        (elevation_df['IrishGrid_Easting'] - easting) ** 2 +
-        (elevation_df['IrishGrid_Northing'] - northing) ** 2
+        (elevation_df['IrishGrid_Easting'] - easting) ** 2 + (elevation_df['IrishGrid_Northing'] - northing) ** 2
     )
 
     # Find the row with the minimum distance
@@ -28,6 +28,7 @@ def find_closest_elevation(easting, northing):
 
     # Return the closest row as a dictionary
     return closest_row.to_dict()
+
 
 # Example Easting and Northing to test the function
 easting = 311567  # Replace with the Easting you want to search
